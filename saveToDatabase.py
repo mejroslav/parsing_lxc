a0d50d8dca56e5caf5d8d93063e4f51c4d5bfca4
mejroslav <miroslav.burysek@gmail.com>
2023-01-06 13:46:49 +0100

    now loads addresses too

--- saveToDatabase.py
+++ saveToDatabase.py
@@ -10,7 +10,10 @@ def initialize_MySQL() -> mysql.connector.MySQLConnection:
         database="lxc_containers"
     )
     cursor = database.cursor()
-    create_table = """CREATE TABLE IF NOT EXISTS containers
+    cursor.execute("""DROP TABLE IF EXISTS containers""")
+    cursor.execute("""DROP TABLE IF EXISTS addresses""")
+    
+    create_table = """CREATE TABLE containers
     (name VARCHAR(255),
     cpu_usage INT,
     memory_usage INT,
@@ -21,7 +24,7 @@ def initialize_MySQL() -> mysql.connector.MySQLConnection:
     cursor.execute(create_table)
     
     # IP adresy mohou mít 39
-    create_addresses = """CREATE TABLE IF NOT EXISTS addresses
+    create_addresses = """CREATE TABLE addresses
     (name VARCHAR(255),
     address VARCHAR(39)
     )"""
@@ -34,10 +37,12 @@ def initialize_MySQL() -> mysql.connector.MySQLConnection:
 def save_to_database(database: mysql.connector.MySQLConnection, data: list[LXC_Container]):
     cursor = database.cursor()
     
-    data_tuple = [(x.name, x.cpu, x.memory, x.created_at, x.status) for x in data]
-    
+    containers_tuple = [(x.name, x.cpu, x.memory, x.created_at, x.status) for x in data]
     command = "INSERT INTO containers VALUES (%s, %s, %s, %s, %s)"
+    cursor.executemany(command, containers_tuple)
     
-    cursor.executemany(command, data_tuple)
+    addresses_tuple = [(container.name, address) for container in data for address in container.addresses]
+    command = "INSERT INTO addresses VALUES (%s, %s)"
+    cursor.executemany(command, addresses_tuple)
     
     database.commit()
